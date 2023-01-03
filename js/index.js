6c157cbadb650313a7ade9f0949cc753c9f7aca4
Orkhan22 <orkhanjafarli@Gmail.com>
2023-01-04 01:18:49 +0400

    sss

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function irelislide() {
+  showSlides((slideIndex += 1));
+}
+
+function gerislide() {
+  showSlides((slideIndex -= 1));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("img-1");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
